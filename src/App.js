a84dca667e0e7bb0a7ddcc9dde540657824e79fa
Denis Loza <denisloza.git@gmail.com>
2023-11-27 14:08:19 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './iconLogo.svg';
 import './App.css';
 
 function App() {
@@ -6,16 +6,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/DenisLoza/test-software"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          WELCOME to ICON DESIGN
         </a>
       </header>
     </div>
